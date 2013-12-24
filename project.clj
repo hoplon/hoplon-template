38f368c0683966d4ecbcd930e4d0c66c4cddbef3
Devin Walters <devinw@gmail.com>
2013-12-24 17:43:23 -0600

    Retain decent template formatting

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hoplon/lein-template "1.1.1"
+(defproject hoplon/lein-template "1.1.5"
   :description        "Create new Hoplon projects with style and grace."
   :url                "http://github.com/tailrecursion/hoplon-template/"
   :license            {:name  "Eclipse Public License"
