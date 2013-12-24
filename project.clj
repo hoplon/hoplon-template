a7c6c79831d99d89343cefd3ba255201fa99898c
Devin Walters <devinw@gmail.com>
2013-12-24 17:32:33 -0600

    Fix template to use dependencies key, tiny version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hoplon/lein-template "1.1.0"
+(defproject hoplon/lein-template "1.1.1"
   :description        "Create new Hoplon projects with style and grace."
   :url                "http://github.com/tailrecursion/hoplon-template/"
   :license            {:name  "Eclipse Public License"
