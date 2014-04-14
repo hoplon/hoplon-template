75fa9f8b2edef782eb7a40492d42e16d01c78894
Micha Niskin <micha.niskin@gmail.com>
2014-04-14 17:15:53 -0400

    add resources/public to gitignore

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hoplon/lein-template "2.2.2"
+(defproject hoplon/lein-template "2.2.3"
   :description        "Create new Hoplon projects with style and grace."
   :url                "http://github.com/tailrecursion/hoplon-template/"
   :license            {:name  "Eclipse Public License"
