048dda4b22eda4e67743a282bff15b64e639d87d
mynomoto <mynomoto@gmail.com>
2014-08-18 19:57:07 -0300

    Update ancient-clj

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url                "http://github.com/tailrecursion/hoplon-template/"
   :license            {:name  "Eclipse Public License"
                        :url   "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[ancient-clj "0.1.6"]]
+  :dependencies [[ancient-clj "0.1.10"]]
   :eval-in-leiningen true)
