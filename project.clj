85fad1c826efcbda85208d64f4fb09f1a6ccbf15
Alan Dipert <alan@dipert.org>
2015-10-02 15:58:15 -0400

    rm project.clj

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject hoplon/lein-template "3.2.0"
-  :description        "Create new Hoplon projects with style and grace."
-  :url                "http://github.com/tailrecursion/hoplon-template/"
-  :license            {:name  "Eclipse Public License"
-                       :url   "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[ancient-clj "0.3.11" :exclusions [com.amazonaws/aws-java-sdk-s3]]
-                 [com.amazonaws/aws-java-sdk-s3 "1.9.0" :exclusions [joda-time]]]
-  :eval-in-leiningen true)
