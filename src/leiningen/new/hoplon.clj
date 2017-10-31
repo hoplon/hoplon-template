2bb998a73dc4b79688bc980dbdca2b45e278819e
Thorben Stangenberg <thorben@stangenberg.ch>
2017-10-31 22:04:28 +0100

    update dependencies versions

--- src/leiningen/new/hoplon.clj
+++ src/leiningen/new/hoplon.clj
@@ -5,13 +5,13 @@
 (defn hoplon
   "Create new Hoplon project."
   [name]
-  (let [boot-cljs-v "1.7.228-2"
-        boot-core-v "2.7.1"
+  (let [boot-cljs-v "2.1.4"
+        boot-core-v "2.7.2"
         boot-jetty-v "0.1.3"
-        boot-reload-v "0.4.13"
+        boot-reload-v "0.5.2"
         clojure-v "1.8.0"
-        clojurescript-v "1.9.293"
-        hoplon-v "6.0.0-alpha17"
+        clojurescript-v "1.9.946"
+        hoplon-v "7.0.3"
         render  (t/renderer "hoplon")
         data    {:raw-name         name
                  :boot-cljs-v      boot-cljs-v
